f32843652cfb3725d92f52ebd576dd78148c7857
Alexander Manzyuk <admsev@gmail.com>
2019-04-18 09:24:46 +0200

    DRY, Isolate stats_presenter_spec.rb

--- spec/ruby_app/stats_presenter_spec.rb
+++ spec/ruby_app/stats_presenter_spec.rb
@@ -2,22 +2,12 @@ require 'spec_helper'
 
 describe StatsPresenter do
   let(:presenter) { described_class.new(stats) }
-  let(:stats) { Stats.new(file_name) }
 
-  let(:file_name) { 'webserver.log' }
-  let(:webserver_log_content) do
-    StringIO.new <<~TXT
-      /help_page/1 126.318.035.038
-      /contact 184.123.665.067
-      /help_page/1 126.318.035.039
-      /contact 184.123.665.067
-      /contact 184.123.665.068
-      /contact 184.123.665.069
-    TXT
-  end
-
-  before do
-    allow(File).to receive(:open).with(file_name, 'r').and_return(webserver_log_content)
+  let(:stats) do
+    double(
+      visits:       [['/contact', 4], ['/help_page/1', 2]],
+      unique_views: [['/contact', 3], ['/help_page/1', 2]]
+    )
   end
 
   describe '#visits' do
